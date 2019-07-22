c4ad9fba3000862fafe05841fb35c137df6613a5
Renée Cruz <reneebasscruz@gmail.com>
2019-07-22 21:43:25 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -27,6 +27,11 @@ def fourth_challenge
             :age => "30",
             :attitude => "confused"
           }
+          {
+            :name => "Nurse",
+            :age => "44",
+            :attitude => "worried"
+          }
       ]
    }
 }

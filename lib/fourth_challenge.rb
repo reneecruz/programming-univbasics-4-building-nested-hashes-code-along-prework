d2dd28e6eced03e1aaccc51fdddbc29355854d21
Renée Cruz <reneebasscruz@gmail.com>
2019-07-22 21:39:59 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,6 +9,11 @@ def fourth_challenge
         :name => "Benvolio",
         :age => "17",
         :attitude => "worried"
+        },
+        {
+        :name => "Mercutio",
+        :age => "17",
+        :attitude => "hot-headed"
         }
       ]
    }, 
